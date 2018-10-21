dab49ffc1a5c657c8c6c33bdc901aa5a50a75c06
Pauline Kelly <pauline.m.kelly1@gmail.com>
2018-10-21 17:40:54 +1100

    Create Cell model

--- java/src/main/java/minesweeper/model/Cell.java
+++ java/src/main/java/minesweeper/model/Cell.java
@@ -1,7 +1,38 @@
 package minesweeper.model;
 
 class Cell {
+    private final boolean hasMine;
+    private final int mineCount;
 
-    Cell() {
+    private boolean isVisible;
+    private boolean hasFlag;
+
+    private Cell(boolean hasMine, int mineCount) {
+        this.hasMine = hasMine;
+        this.mineCount = mineCount;
+    }
+
+    static Cell createMine() {
+        return new Cell(/* hasMine= */ true, /* mineCount= */ 0);
+    }
+
+    static Cell createMineCount(int mineCount) {
+        return new Cell(/* hasMine= */ false, mineCount);
+    }
+
+    boolean isVisible() {
+        return isVisible;
+    }
+
+    void makeVisible() {
+        isVisible = true;
+    }
+
+    boolean hasFlag() {
+        return hasFlag;
+    }
+
+    void setHasFlag(boolean hasFlag) {
+        this.hasFlag = hasFlag;
     }
 }
